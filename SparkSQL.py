0e51d964aacdc3e21011836d97f2a4003d8685e2
Simon-lsy <lusiyuan666@163.com>
2018-12-04 19:15:19 +0800

    Test of SparkSQL with people.txt using createDataFrame

---
+++ SparkSQL.py
@@ -0,0 +1,32 @@
+from pyspark import SparkContext
+from pyspark import SQLContext
+from pyspark.sql.types import StructField, StringType, FloatType, StructType, IntegerType
+from pyspark.sql import Row
+
+
+sc = SparkContext(appName='SparkSQL')
+
+sparksql = SQLContext(sc)
+
+peopleRDD = sc.textFile("C:/Users/Admin/PycharmProjects/Spark/people.txt").map(lambda line: line.split(", "))
+
+# row = Row("val")
+# df = peopleRDD.map(row).toDF()
+# df.show()
+# print(df)
+
+schema = StructType([
+    StructField("name", StringType(), True),
+    StructField("age", StringType(), True)])
+
+df = sparksql.createDataFrame(peopleRDD, schema)
+df.show()
+
+# df.filter(df.age > 19).show()
+
+
+df.printSchema()
+
+df.createOrReplaceTempView("People")
+sparksql.sql("SELECT SUM(age) AS ageTotal FROM People").show()
+
